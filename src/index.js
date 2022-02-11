cbaa1d8ab91cf56afb11caa761e40c2ce753085f
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-02-11 22:18:08 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig){
+    let openBracket = [];
+    let closeBracket = [];
+    let commonBracket = [];
+    let sameBracket = [];
+    bracketsConfig.forEach(array => { array[0] === array[1] ? sameBracket.push(array[0]) : commonBracket.push(array);
+    });
+    commonBracket.forEach(array => { 
+        openBracket.push(array[0])
+        closeBracket.push(array[1]);
+    });
+    
+    let stack = [];
+     for (let i = 0; i < str.length; i++) {
+      if ( openBracket.includes(str[i])) {
+       stack.push(str[i]);
+       }
+      else if (closeBracket.includes(str[i])){
+        if (stack.length !== 0 ){
+           for (let j = 0; j < openBracket.length; j++) {
+            let stackItem = stack[stack.length - 1];
+            if (stackItem === openBracket[j] && str[i] === closeBracket[j]) {
+              stack.pop();
+            }
+          } 
+        } else return false;
+      }
+      else if (sameBracket.includes(str[i])) {
+        stack.push(str[i]);
+        let stackItem = stack[stack.length - 1];
+        if (stack.length >= 2 && stackItem === stack[stack.length - 2]) {
+            stack.pop();
+            stack.pop();
+        }
+      }
+    }
+    return stack.length === 0;
+  }
 }
